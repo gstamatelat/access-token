23554d8d2b2efa51834f07d4011299dbb7bbc87e
Giorgos Stamatelatos <gstamatelat@gmail.com>
2022-08-24 19:39:14 +0000

    Change to authoritzation using redirect URI

--- access_token.py
+++ access_token.py
@@ -1,6 +1,10 @@
 import argparse
+import http.server
+import json
+from urllib import parse
 
 import requests
+
 
 parser = argparse.ArgumentParser(description='Generate access and refresh tokens for Google APIs.',
                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -18,25 +22,48 @@ if args.scope is None:
 
 print()
 
-oauth = (
-    "https://accounts.google.com/o/oauth2/v2/auth?"
-    "scope={scope}"
-    "&access_type=offline"
-    "&response_type=code"
-    "&client_id={client_id}"
-    "&redirect_uri=urn:ietf:wg:oauth:2.0:oob"
-).format(scope=args.scope, client_id=args.client_id)
+# Construct the URL
 
-print("Open this URL in your browser and follow the steps:\n\n{}\n".format(oauth))
+oauth = "https://accounts.google.com/o/oauth2/v2/auth?" \
+    f"scope={args.scope}" \
+    "&access_type=offline" \
+    "&response_type=code" \
+    f"&client_id={args.client_id}" \
+    "&redirect_uri=http://localhost"
 
-authorization_code = input("Paste the code here: ")
+print(f"Open this URL in your browser and follow the steps:\n\n{oauth}\n")
+
+# Receive authorization code
+
+class Handler(http.server.BaseHTTPRequestHandler):
+    authorization_code = None
+    def do_GET(self):
+        parameters = parse.parse_qs(parse.urlparse(self.path).query)
+        if 'code' in parameters:
+            self.send_response(200, "OK")
+            self.end_headers()
+            Handler.authorization_code = parameters['code']
+            self.wfile.write("You can close this tab now and switch to the terminal.".encode("utf-8"))
+        else:
+            self.send_response(200, "OK")
+            self.end_headers()
+            self.wfile.write("Didn't receive authorization code.".encode("utf-8"))
+with http.server.HTTPServer(("localhost", 80), Handler) as httpd:
+    while not Handler.authorization_code:
+        httpd.handle_request()
+
+authorization_code = Handler.authorization_code
+
+print()
+
+# Retrieve access and refresh tokens
 
 tokens = requests.post("https://www.googleapis.com/oauth2/v4/token",
                        params={
                            'code': authorization_code,
                            'client_id': args.client_id,
                            'client_secret': args.client_secret,
-                           'redirect_uri': 'urn:ietf:wg:oauth:2.0:oob',
+                           'redirect_uri': 'http://localhost',
                            'grant_type': 'authorization_code'
                        },
                        headers={
@@ -44,5 +71,4 @@ tokens = requests.post("https://www.googleapis.com/oauth2/v4/token",
                        }
                        ).json()
 
-print()
-print(tokens)
+print(json.dumps(tokens, indent=4))
